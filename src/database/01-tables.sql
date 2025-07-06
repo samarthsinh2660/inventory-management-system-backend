1023d6bf114835dad35a46f32003be2c349cd66f
samarthsinh2660 <rajuvala80@gmail.com>
2025-07-06 19:14:17 +0530

    update the notification

--- src/database/01-tables.sql
+++ src/database/01-tables.sql
@@ -101,7 +101,6 @@ CREATE TABLE StockAlerts (
 -- Notifications Table
 CREATE TABLE Notifications (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
     product_id INT NOT NULL,
     stock_alert_id INT,
     message VARCHAR(255) NOT NULL,
@@ -109,7 +108,6 @@ CREATE TABLE Notifications (
     min_threshold FLOAT NOT NULL,
     is_read BOOLEAN DEFAULT false,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE CASCADE,
     FOREIGN KEY (stock_alert_id) REFERENCES StockAlerts(id) ON DELETE SET NULL
 );
