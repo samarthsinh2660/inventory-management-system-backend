4474897f59b094ea1f52fd5bfa486f0bc4f2e86b
samarthsinh2660 <rajuvala80@gmail.com>
2025-08-16 17:49:04 +0530

    store logs locally

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,32 @@
 import winston from "winston";
+import fs from 'fs';
+
+// Ensure logs directory exists
+if (!fs.existsSync('logs')) {
+    fs.mkdirSync('logs');
+}
 
 function createLogger(label: string): winston.Logger {
     return winston.createLogger({
         level: 'debug',
-        format: winston.format.combine( 
+        format: winston.format.combine(
             winston.format.label({ label }),
-            winston.format(info => {
-                info.level = info.level.toUpperCase()
-                return info;
-            })(),
-            winston.format.colorize(),
-            winston.format.simple(),
             winston.format.timestamp({
                 format: 'YYYY-MM-DD HH:mm:ss'
-            }),
-            winston.format.printf(info => `[${info.label}] ${info.timestamp} ${info.level}: ${info.message}`+(info.splat!==undefined?`${info.splat}`:" "))
+            })
         ),
         transports: [
-            new winston.transports.Console(),
-            new winston.transports.File({ filename: 'error.log', level: 'debug' }),
+            new winston.transports.Console({
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.printf(info => `[${info.label}] ${info.timestamp} ${info.level.toUpperCase()}: ${info.message}` + (info.splat !== undefined ? `${info.splat}` : " "))
+                )
+            }),
+            new winston.transports.File({
+                filename: 'logs/error.log',
+                level: 'debug',
+                format: winston.format.printf(info => `[${info.label}] ${info.timestamp} ${info.level}: ${info.message}` + (info.splat !== undefined ? `${info.splat}` : " "))
+            }),
         ]
     });
 }
